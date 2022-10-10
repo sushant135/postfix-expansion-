93258f7ac99d3cbfc46a5d59391eaf34c2a379e8
Sushantfox <115439873+Sushantfox@users.noreply.github.com>
2022-10-10 08:33:47 +0530

    Update postfixexapan.c

--- postfixexapan.c
+++ postfixexapan.c
@@ -1,65 +1,57 @@
-#include<stdio.h>
-#include<ctype.h>
-int stack[20];
-int top = -1;
-
-void push(int x)
+import java.util.Stack;
+ 
+public class Test
 {
-    stack[++top]=x;
-}
-
-int pop()
-{
-    return stack[top--];
-}
-
-int main(){    
- char exp[20];
-    char *e;
-    int n1,n2,n3,num;
-    printf("enter the expression :");
-    scanf("%s",exp);
-    e=exp;
-    while(*e !='\0')
-{
-  if(isdigit(*e))
-  {
- num =*e-48;
- push(num);
- }
- else 
- {
- n1 =pop();
-n2=pop();
- switch(*e)
-{
- case '+':
- {
-n3 = n2 + n1;
-break;
-}
-case '-':
-{
-n3 = n2- n1;
-break;
-}
-case '*':
- {
- n3 = n2 * n1;
-break;
-
-   }
-case '/':
-  {
-   n3 =n2/n1;
-  break;
-   }
-  }
-   push(n3);
-
-   }
-    e++;
- }
- printf("\nthe result of expression  %s = %d\n\n",exp,pop());
-return 0;
+    // Method to evaluate value of a postfix expression
+    static int evaluatePostfix(String exp)
+    {
+        //create a stack
+        Stack<Integer> stack=new Stack<>();
+         
+        // Scan all characters one by one
+        for(int i=0;i<exp.length();i++)
+        {
+            char c=exp.charAt(i);
+             
+            // If the scanned character is an operand (number here),
+            // push it to the stack.
+            if(Character.isDigit(c))
+            stack.push(c - '0');
+             
+            //  If the scanned character is an operator, pop two
+            // elements from stack apply the operator
+            else
+            {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+                 
+                switch(c)
+                {
+                    case '+':
+                    stack.push(val2+val1);
+                    break;
+                     
+                    case '-':
+                    stack.push(val2- val1);
+                    break;
+                     
+                    case '/':
+                    stack.push(val2/val1);
+                    break;
+                     
+                    case '*':
+                    stack.push(val2*val1);
+                    break;
+              }
+            }
+        }
+        return stack.pop();   
+    }
+     
+    // Driver program to test above functions
+    public static void main(String[] args)
+    {
+        String exp="231*+9-";
+        System.out.println("postfix evaluation: "+evaluatePostfix(exp));
+    }
 }
