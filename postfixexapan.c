47ee33e56b6c48a5d2eaa27b21bc37b2a703f4a1
Sushantfox <115439873+Sushantfox@users.noreply.github.com>
2022-10-10 08:36:45 +0530

    Update postfixexapan.c

--- postfixexapan.c
+++ postfixexapan.c
@@ -1,57 +1,104 @@
-import java.util.Stack;
- 
-public class Test
+
+#include <stdio.h>
+#include <ctype.h>
+#define MAXSTACK 100 
+#define POSTFIXSIZE 100 
+int stack[MAXSTACK];
+int top = -1; 
+void push(int item)
 {
-    // Method to evaluate value of a postfix expression
-    static int evaluatePostfix(String exp)
-    {
-        //create a stack
-        Stack<Integer> stack=new Stack<>();
-         
-        // Scan all characters one by one
-        for(int i=0;i<exp.length();i++)
-        {
-            char c=exp.charAt(i);
-             
-            // If the scanned character is an operand (number here),
-            // push it to the stack.
-            if(Character.isDigit(c))
-            stack.push(c - '0');
-             
-            //  If the scanned character is an operator, pop two
-            // elements from stack apply the operator
-            else
-            {
-                int val1 = stack.pop();
-                int val2 = stack.pop();
-                 
-                switch(c)
-                {
-                    case '+':
-                    stack.push(val2+val1);
-                    break;
-                     
-                    case '-':
-                    stack.push(val2- val1);
-                    break;
-                     
-                    case '/':
-                    stack.push(val2/val1);
-                    break;
-                     
-                    case '*':
-                    stack.push(val2*val1);
-                    break;
-              }
-            }
+
+    if (top >= MAXSTACK - 1) {
+        printf("stack over flow");
+        return;
+    }
+    else {
+        top = top + 1;
+        stack[top] = item;
+    }
+}
+
+int pop()
+{
+    int item;
+    if (top < 0) {
+        printf("stack under flow");
+    }
+    else {
+        item = stack[top];
+        top = top - 1;
+        return item;
+    }
+}
+
+void EvalPostfix(char postfix[])
+{
+
+    int i;
+    char ch;
+    int val;
+    int A, B;
+
+  
+    for (i = 0; postfix[i] != ')'; i++) {
+        ch = postfix[i];
+        if (isdigit(ch)) {
+       
+            push(ch - '0');
         }
-        return stack.pop();   
+        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+
+            A = pop();
+            B = pop();
+
+            switch (ch)
+            {
+            case '*':
+                val = B * A;
+                break;
+
+            case '/':
+                val = B / A;
+                break;
+
+            case '+':
+                val = B + A;
+                break;
+
+            case '-':
+                val = B - A;
+                break;
+            }
+
+            push(val);
+        }
     }
-     
-    // Driver program to test above functions
-    public static void main(String[] args)
-    {
-        String exp="231*+9-";
-        System.out.println("postfix evaluation: "+evaluatePostfix(exp));
+    printf(" \n Result of expression evaluation : %d \n", pop());
+}
+
+int main()
+{
+
+    int i;
+
+   
+    char postfix[POSTFIXSIZE];
+    printf("ASSUMPTION: There are only four operators(*, /, +, -) in an expression and operand is single digit only.\n");
+    printf(" \nEnter postfix expression,\npress right parenthesis ')' for end expression : ");
+
+
+
+    for (i = 0; i <= POSTFIXSIZE - 1; i++) {
+        scanf("%c", &postfix[i]);
+
+        if (postfix[i] == ')') 
+        {
+            break;
+        } 
     }
+
+
+    EvalPostfix(postfix);
+
+    return 0;
 }
